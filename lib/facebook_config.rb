1c3f8bce7368e41ac7bb0f0053d1c14763fd2865
Jimmy Kittiyachavalit <jimmy@causes.com>
2010-11-19 10:47:19 -0800

    Use env vars for facebook.yml.
    It's easier to manage multiple instances on heroku when the configs are
    not static files. Using environment variables is the current best
    practice.

--- lib/facebook_config.rb
+++ lib/facebook_config.rb
@@ -1,10 +1,10 @@
 class FacebookConfig
   CANVAS_BASE = "http://apps.facebook.com/"
+  CONFIG_FILE = File.join(Rails.root, "config", "facebook.yml")
 
   def self.method_missing(m)
     @config ||= begin
-      cfg = YAML.load_file(File.join(Rails.root, "config", "facebook.yml"))
-      cfg[Rails.env]
+      YAML::load(ERB.new(File.read(CONFIG_FILE)).result)
     end
     @config[m.to_s]
   end
