0b779ea1073346ace00a26419922989f88828f71
Chris Chan <chris@causes.com>
2011-05-30 01:55:59 -0700

    Decompose.

--- app/models/campfire.rb
+++ app/models/campfire.rb
@@ -2,17 +2,31 @@ class Campfire
   KEYS = ["campfire_subdomain", "campfire_token", "campfire_room"]
 
   def self.notify_mock_created(mock, url)
-    options = {
+    if room
+      room.speak "#{mock.author.name} posted a new mock:"
+      room.speak mock.image.url
+      room.speak url
+    end
+  rescue Tinder::AuthenticationFailed, # Invalid token
+         NoMethodError                 # Invalid subdomain
+    # Do nothing.
+  end
+
+  def self.room
+    @room ||= begin
+      campfire =
+        Tinder::Campfire.new settings["campfire_subdomain"], self.options
+      campfire.find_room_by_name(settings["campfire_room"])
+    end
+  end
+
+  def self.options
+    {
       :ssl => true,
       :token => settings["campfire_token"]
     }
-    campfire = Tinder::Campfire.new settings["campfire_subdomain"], options
-    room = campfire.find_room_by_name(settings["campfire_room"])
-    room.speak "#{mock.author.name} posted a new mock:"
-    room.speak mock.image.url
-    room.speak url
   end
-  
+
   def self.settings
     @settings ||= begin
       settings = {}
