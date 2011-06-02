2228c366f490a4151bbe69edd16067251eaa0916
Chris Chan <chris@causes.com>
2011-06-02 16:46:36 -0700

    Fix a bug in which Campfire settings won't properly save; this is due to the fact that the room and settings are permanently memoized in a production environment.

--- app/models/campfire.rb
+++ app/models/campfire.rb
@@ -2,6 +2,7 @@ class Campfire
   KEYS = ["campfire_subdomain", "campfire_token", "campfire_room"]
 
   def self.notify_mock_created(mock, url)
+    room = find_room
     if use? && room
       room.speak "#{mock.author.name} posted a new mock:"
       room.speak mock.image.url
@@ -12,12 +13,9 @@ class Campfire
     # Do nothing.
   end
 
-  def self.room
-    @room ||= begin
-      campfire =
-        Tinder::Campfire.new settings["campfire_subdomain"], self.options
-      campfire.find_room_by_name(settings["campfire_room"])
-    end
+  def self.find_room
+    campfire = Tinder::Campfire.new settings["campfire_subdomain"], self.options
+    campfire.find_room_by_name(settings["campfire_room"])
   end
 
   def self.options
@@ -28,13 +26,11 @@ class Campfire
   end
 
   def self.settings
-    @settings ||= begin
-      settings = {}
-      KEYS.each do |key|
-        settings[key] = Setting.find_or_create_by_key(key).value
-      end
-      settings
+    settings = {}
+    KEYS.each do |key|
+      settings[key] = Setting[key]
     end
+    settings
   end
   
   def self.use?
