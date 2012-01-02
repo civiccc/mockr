3c25d38de606f777707708b0f24ffabcfdce16e0
Chris Chan <chris@causes.com>
2012-01-02 02:16:37 -0500

    You can now add the next version of a mock by dragging it onto a mock page

---
+++ public/javascripts/mocks/show.js
@@ -0,0 +1,35 @@
+$(function() {
+    $('#mock_creation_form').fileupload({
+        dataType: 'json',
+        url: '/mocks',
+        singleFileUploads: false,
+        done: function (e, data) {
+          result = data.result;
+          if (result.mocks_count == 1) {
+            window.location = '/mocks/' + result.mock_ids[0];
+          } else {
+            window.location = '/projects/' + result.project_id;
+          }
+        },
+        start: function (e) {
+          $(this).addClass('submitted');
+        },
+        fail: function (e, data) {
+          window.location = window.location + '?failed=1';
+        }
+    });
+
+    var dragCount = 0;
+
+    $(document.body).bind('dragenter', function(e) {
+      dragCount += 1;
+      $(document.body).addClass("dragging");
+      $("#mock_creation_form").height($(window).height());
+    });
+    $(document.body).bind('dragleave', function(e) {
+      dragCount -= 1;
+      if (dragCount == 0) {
+        $(document.body).removeClass("dragging");
+      }
+    });
+});
