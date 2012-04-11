ed0d09de5595d2d3aee024155c4e99277cb13584
Adam Derewecki <adam@causes.com>
2012-04-11 09:08:06 -0700

    Use OpenGraph to get new user's name in claim flow
    Instead of a "Lost" inspired default name, we utilize the Facebook OpenGraph
    JSON API to pull basic contact details based on a Facebook UID.
    
    Change-Id: I78ed6f37f34aad081ed2994bf406f7c37defffcb
    Reviewed-on: https://gerrit.causes.com/5113
    Reviewed-by: Lann Martin <lann@causes.com>
    Tested-by: Adam Derewecki <adam@causes.com>

--- app/controllers/claim_controller.rb
+++ app/controllers/claim_controller.rb
@@ -1,12 +1,18 @@
+
 class ClaimController < ApplicationController
 
   skip_before_filter :require_authorization
 
   def index
     if Mockr.unclaimed? && viewer.authenticated?
+      graph_data = ActiveSupport::JSON.decode(
+        open("http://graph.facebook.com/#{viewer.facebook_uid}").read)
       owner = User.create(:facebook_uid => viewer.facebook_uid,
-        :name => "Jacob")
-      flash[:notice] = "Congratulations #{owner.first_name}, you're now the proud owner of this brand new instance of Mockr!"
+                          :name => graph_data['name'])
+      flash[:notice] = <<-EOS.squish
+        Congratulations #{owner.first_name}, you're now the proud owner of this
+        brand new instance of Mockr!
+      EOS
       redirect_to root_path
     else
       redirect_to new_session_path
