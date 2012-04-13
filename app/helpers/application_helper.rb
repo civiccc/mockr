90d1dbb53d8dbe80f573f3282fb5b71db157ed13
Lann Martin <lann@causes.com>
2012-04-13 15:29:07 -0700

    Some general cleanup
    Change-Id: Iab63c559b2fc200f3e7bf12d1f302747a468820f
    Reviewed-on: https://gerrit.causes.com/5267
    Reviewed-by: Lann Martin <lann@causes.com>
    Tested-by: Lann Martin <lann@causes.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def format_text(text)
-    text = nl2br(text)
+    text = nl2br(h text)
     auto_link(text, :all, :target => "_blank").html_safe
   end
 
