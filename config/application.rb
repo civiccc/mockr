c401d43c500e7c991815c544bb8298dc5c0f8cf7
Adam Derewecki <adam@causes.com>
2012-04-13 18:17:28 -0700

    Remove deprecated action_controller.session call
    Change-Id: I8a7132e11bb2b1fa5714ad6dd76dce1043d0c431
    Reviewed-on: https://gerrit.causes.com/5302
    Reviewed-by: Jimmy Kittiyachavalit <jimmy@causes.com>
    Tested-by: Adam Derewecki <adam@causes.com>

--- config/application.rb
+++ config/application.rb
@@ -39,12 +39,6 @@ module Mockr
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
-    config.action_controller.session = {
-      :session_key => '_mockr_session',
-      :secret      => '21ee28b973e26bef6f112ae1807d3ff3eafeebadb1f0ccf70b098451ba139e24d5de6508045291bcca481317969c3852241ed097c1d2431d80407e268f74731e',
-      :session_expires => Time.now + 31557600 #1.year not yet defined
-    }
-
     config.action_mailer.default_url_options = {:host => (ENV['APP_HOST'] || "mockr")}
   end
 end
