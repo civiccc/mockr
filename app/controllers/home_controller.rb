18f3737d897234833d974592a24ff36209f11d61
Aiden Scandella <aiden@causes.com>
2013-02-16 12:53:33 -0800

    Fix Ruby 1.8 chaining syntax
    The app was crashing on Heroku due to a syntax error. This fixes that.
    
    Change-Id: Ife8f6f2e6038cbd39ca3e00317aa6330f6f43807
    Reviewed-on: https://gerrit.causes.com/18765
    Tested-by: Aiden Scandella <aiden@causes.com>
    Reviewed-by: Aiden Scandella <aiden@causes.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,12 @@
 class HomeController < ApplicationController
   def index
-    @recent = Mock.joins(:mock_views).where('mock_views.user_id = ?', viewer.id)
-      .order('mock_views.viewed_at DESC').limit(3)
-  end
-  
-  def mock_set
-    render :partial => "recent_mock_set", :locals => {:page => params[:page]}
+    @recent = Mock.joins(:mock_views).
+      where('mock_views.user_id = ?', viewer.id).
+      order('mock_views.viewed_at DESC').
+      limit(3)
   end
 
+  def mock_set
+    render :partial => 'recent_mock_set', :locals => {:page => params[:page]}
+  end
 end
